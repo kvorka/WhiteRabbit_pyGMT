06f13afb7b051904dffa8c839ad0fcc2c90c1264
kvorka <jkdarkorbit@gmail.com>
2024-10-07 01:51:53 +0200

    more minor updates

--- harm_synthesis/modules/harmsy/smod/harmsy@harmsy.f90
+++ harm_synthesis/modules/harmsy/smod/harmsy@harmsy.f90
@@ -5,10 +5,10 @@ submodule (harmsy) harmsy
     integer,           intent(in)  :: jmax, n
     complex(kind=dbl), intent(in)  :: spectra(n,*)
     real(kind=dbl),    intent(out) :: gridvals(n,2*nth,0:nth)
-    integer                        :: it, ip, ij, im, in
+    integer                        :: it, ip, ij, im, in, ijm, imj
     real(kind=dbl),    allocatable :: p0j(:), pmj(:), pmj1(:), pmj2(:), costheta(:), sintheta(:)
     complex(kind=dbl)              :: expmul
-    complex(kind=dbl), allocatable :: sumLege1(:), sumLege2(:), sumL1(:), sumL2(:)
+    complex(kind=dbl), allocatable :: sumLege1(:), sumLege2(:), sumL1(:), sumL2(:), spectramj(:,:)
     
     !!**************************************************************************************!!
     !!* Prepare output array.                                                              *!!
@@ -24,7 +24,19 @@ submodule (harmsy) harmsy
     allocate( sumLege1(4*n*(jmax+1)), sumL1(4*n), &
             & sumLege2(4*n*(jmax+1)), sumL2(4*n), &
             & p0j(4), pmj(4), pmj1(4), pmj2(4),   &
-            & costheta(4), sintheta(4)            )
+            & costheta(4), sintheta(4),           &
+            & spectramj(n,jmax*(jmax+1)/2+jmax+1) )
+    
+    do im = 0, jmax
+      do ij = im, jmax
+        ijm = ij*(ij+1)/2+im+1
+        imj = im*(jmax+1)-im*(im+1)/2+ij+1
+        
+        do concurrent ( in = 1:n )
+          spectramj(in,imj) = spectra(in,ijm)
+        end do
+      end do
+    end do
     
     !!**************************************************************************************!!
     !!* Main cycle over latitudes. As nth=180, 360, ..., vectorization is handled by com-  *!!
@@ -39,19 +51,24 @@ submodule (harmsy) harmsy
           sumL2(in) = czero
         end do
         
-        ij = im
+        imj = im*(jmax+1)-im*(im+1)/2+im+1
           call pmj4_set_sub( im, sintheta(1), p0j(1), pmj(1), pmj1(1), pmj2(1) )
-          call pmj4_sum_sub( n, pmj, sumL1(1), spectra(1,ij*(ij+1)/2+im+1))
+          call pmj4_sum_sub( n, pmj, sumL1(1), spectramj(1,imj))
         
-        do ij = im+1, jmax
-          call pmj4_recursion_sub( im*(jmax+1)-im*(im+1)/2+ij+1, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+        do ij = 1, (jmax-im)/2
+          imj = imj + 2
           
-          if ( mod(ij+im,2) == 0 ) then
-            call pmj4_sum_sub( n, pmj, sumL1(1), spectra(1,ij*(ij+1)/2+im+1))
-          else
-            call pmj4_sum_sub( n, pmj, sumL2(1), spectra(1,ij*(ij+1)/2+im+1))
-          end if
+          call pmj4_recursion_sub( imj-1, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+          call pmj4_sum_sub( n, pmj, sumL2(1), spectramj(1,imj-1))
+          
+          call pmj4_recursion_sub( imj, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+          call pmj4_sum_sub( n, pmj, sumL1(1), spectramj(1,imj))
         end do
+        
+        if ( mod(jmax-im,2) /= 0 ) then
+          call pmj4_recursion_sub( imj+1, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+          call pmj4_sum_sub( n, pmj, sumL2(1), spectramj(1,imj+1))
+        end if
         
         call pmj4_transpose_sub( n, sumL1(1), sumL2(1), sumLege1(1+4*n*im), sumLege2(1+4*n*im) )
       end do
@@ -83,19 +100,24 @@ submodule (harmsy) harmsy
           sumL2(in) = czero
         end do
         
-        ij = im
+        imj = im*(jmax+1)-im*(im+1)/2+im+1
           call pmj2_set_sub( im, sintheta(1), p0j(1), pmj(1), pmj1(1), pmj2(1) )
-          call pmj2_sum_sub( n, pmj, sumL1(1), spectra(1,ij*(ij+1)/2+im+1))
-        
-        do ij = im+1, jmax
-          call pmj2_recursion_sub( im*(jmax+1)-im*(im+1)/2+ij+1, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+          call pmj2_sum_sub( n, pmj, sumL1(1), spectramj(1,imj))
           
-          if ( mod(ij+im,2) == 0 ) then
-            call pmj2_sum_sub( n, pmj, sumL1(1), spectra(1,ij*(ij+1)/2+im+1))
-          else
-            call pmj2_sum_sub( n, pmj, sumL2(1), spectra(1,ij*(ij+1)/2+im+1))
+          do ij = 1, (jmax-im)/2
+            imj = imj + 2
+            
+            call pmj2_recursion_sub( imj-1, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+            call pmj2_sum_sub( n, pmj, sumL2(1), spectramj(1,imj-1))
+            
+            call pmj2_recursion_sub( imj, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+            call pmj2_sum_sub( n, pmj, sumL1(1), spectramj(1,imj))
+          end do
+          
+          if ( mod(jmax-im,2) /= 0 ) then
+            call pmj2_recursion_sub( imj+1, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+            call pmj2_sum_sub( n, pmj, sumL2(1), spectramj(1,imj+1))
           end if
-        end do
         
         call pmj2_transpose_sub( n, sumL1(1), sumL2(1), sumLege1(1+2*n*im), sumLege2(1+2*n*im) )
       end do
@@ -129,13 +151,15 @@ submodule (harmsy) harmsy
           sumLege1(in+n*im) = czero
         end do
         
-        ij = im
+        imj = im*(jmax+1)-im*(im+1)/2+im+1
           call pmj_set_sub( im, sintheta(1), p0j(1), pmj(1), pmj1(1), pmj2(1) )
-          call pmj_sum_sub( n, pmj(1), sumLege1(1+n*im), spectra(1,ij*(ij+1)/2+im+1))
+          call pmj_sum_sub( n, pmj(1), sumLege1(1+n*im), spectramj(1,imj))
         
         do ij = im+1, jmax
-          call pmj_recursion_sub( im*(jmax+1)-im*(im+1)/2+ij+1, costheta(1), pmj(1), pmj1(1), pmj2(1) )
-          call pmj_sum_sub( n, pmj(1), sumLege1(1+n*im), spectra(1,ij*(ij+1)/2+im+1))
+          imj = imj + 1
+          
+          call pmj_recursion_sub( imj, costheta(1), pmj(1), pmj1(1), pmj2(1) )
+          call pmj_sum_sub( n, pmj(1), sumLege1(1+n*im), spectramj(1,imj))
         end do
       end do
       
@@ -157,7 +181,8 @@ submodule (harmsy) harmsy
     !!**************************************************************************************!!
     !!* Cleaning after the computation.                                                    *!!
     !!**************************************************************************************!!
-    deallocate( sumLege1, sumLege2, sumL1, sumL2, p0j, pmj, pmj1, pmj2, costheta, sintheta )
+    deallocate( sumLege1, sumLege2, sumL1, sumL2, p0j, pmj, pmj1, pmj2, costheta, sintheta, &
+              & spectramj )
     
   end subroutine harmsy_sub
   
